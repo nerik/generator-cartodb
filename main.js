2ebda6ca1a5270ed89f0f47994cacd81342c4159
nerik <erik.escoffier@gmail.com>
2015-12-29 17:31:18 +0100

    Initial gistup commit.

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log('pouet');
+
+setInterval(a => {
+  console.log('lala')
+})
---
+++ ~main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+console.log('pouet');
+
+setInterval(function (a) {
+  console.log('lala');
+});
